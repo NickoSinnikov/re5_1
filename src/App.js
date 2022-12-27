10078f971e95ad9bb82aa6969fcc7fdcbc04140e
Nick Sinnikov <nickosinnikov@outlook.com>
2022-12-27 18:12:08 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Card from './components/Card';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Card className="card">
+        <CardTitle>Card title</CardTitle>
+        <CardContent>Some quick example text to build on the card title and make up the bulk of the card's content.</CardContent>
+        <CardButton>Go somewhere</CardButton>
+        </Card>
     </div>
   );
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Card(props){
+    return (
+        <div className="card">
+<h2 className="card__title">{title}</h2>
+        </div>
+    )
+}
